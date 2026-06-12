94fa3bf68a9b76812cd45fcc8b9b08513d983c3c
bartenderOG <bartenderback@gmail.com>
2026-06-12 20:45:58 +0600

    ls.6

---
+++ lessons/lesson.6.py
@@ -0,0 +1,11 @@
+# class Hero:
+#     def __init__(self, name):
+#         self.name = name
+from linecache import clearcache
+
+# from colorama import Fore, Back, Style
+# print(Fore.RED + 'some red text')
+# print(Back.GREEN + 'and with a green background')
+# print(Style.DIM + 'and in dim text')
+# print(Style.RESET_ALL)
+# print('back to normal now')
